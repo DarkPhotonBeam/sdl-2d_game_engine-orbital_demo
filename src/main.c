f13bf500434c0344d8f329d0b0385b844d1a6b50
Daniel Leuthold <darkphotonbeam@gmail.com>
2025-11-19 01:56:01 +0100

    Removed gamepad code, Removed some log calls

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#define SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS "SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -30,8 +29,6 @@ void refresh_warp_buf() {
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_GAMEPAD);
-
     Game_AppState *app_state = Game_AppState_Create();
 
     app_state->cam->zoom = 0.5;
@@ -198,15 +195,11 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         SDL_Log("r.x = %f, r.y = %f", r.x, r.y);
     } else if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-    } else if (event->type == SDL_EVENT_GAMEPAD_BUTTON_DOWN) {
-        SDL_Log("Gamepad Button down Event!");
     } else if (event->type == SDL_EVENT_MOUSE_WHEEL) {
-        SDL_Log("Scrollwheel Event x: %f, y: %f", event->wheel.x, event->wheel.y);
         constexpr double min_zoom = 0.001;
         const double amt = event->wheel.y * game->cam->zoom * 0.5;
         game->cam->zoom += amt;
         if (game->cam->zoom < min_zoom) game->cam->zoom = min_zoom;
-        SDL_Log("zoom_diff: %f, new zoom: %f", amt, game->cam->zoom);
     }
     return SDL_APP_CONTINUE;
 }
